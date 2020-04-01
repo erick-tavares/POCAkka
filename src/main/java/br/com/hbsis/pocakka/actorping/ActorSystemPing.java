9d98d0d643f53512bb55a5341180a9aec79fb6a6
Erick E. Tavares <erick-tavares@hotmail.com>
2020-04-01 16:08:15 -0300

    implementanto exemplo com spring

--- src/main/java/br/com/hbsis/pocakka/actorping/ActorSystemPing.java
+++ src/main/java/br/com/hbsis/pocakka/actorping/ActorSystemPing.java
@@ -3,17 +3,33 @@ package br.com.hbsis.pocakka.actorping;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import br.com.hbsis.pocakka.MailBox;
-import br.com.hbsis.pocakka.actorping.ActorPing;
+import br.com.hbsis.pocakka.config.SpringExtension;
+import br.com.hbsis.pocakka.config.SpringProps;
 import com.typesafe.config.ConfigFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ActorSystemPing {
 
-    public static void main(String[] args) {
-        ActorSystem actorSystem = ActorSystem.create("ActorSystemPing", ConfigFactory.load().getConfig("ActorSystemPing"));
+    @Autowired
+    private ApplicationContext context;
 
-        ActorRef actorPingRef = actorSystem.actorOf(ActorPing.props(), "actorPing");
+    public static void main(String[] args) {
+        SpringApplication.run(ActorSystemPing.class, args);
+    }
+
+    @PostConstruct
+    void init() {
+        ActorSystem actorSystem = ActorSystem.create("ActorSystemPing", ConfigFactory.load().getConfig("ActorSystemPing"));
+        SpringExtension.getInstance().get(actorSystem).initialize(context);
+
+        // ActorRef actorPingRef = actorSystem.actorOf(ActorPing.props(), "actorPing");
+        ActorRef actorPingRef = actorSystem.actorOf(SpringProps.create(actorSystem, ActorPing.class), "actorPing");
 
         actorPingRef.tell(new MailBox.PingMensagem("Mensagem"), ActorRef.noSender());
 
--- src/main/java/br/com/hbsis/pocakka/actorpong/ActorSystemPong.java
+++ src/main/java/br/com/hbsis/pocakka/actorpong/ActorSystemPong.java
@@ -2,18 +2,35 @@ package br.com.hbsis.pocakka.actorpong;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import br.com.hbsis.pocakka.actorpong.ActorPong;
+import br.com.hbsis.pocakka.config.SpringExtension;
+import br.com.hbsis.pocakka.config.SpringProps;
 import com.typesafe.config.ConfigFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ActorSystemPong {
 
+    @Autowired
+    private ApplicationContext context;
+
     public static void main(String[] args) {
+        SpringApplication.run(ActorSystemPong.class, args);
+    }
+
+    @PostConstruct
+    void init() {
         ActorSystem actorSystem = ActorSystem.create("ActorSystemPong", ConfigFactory.load().getConfig("ActorSystemPong"));
 
-        ActorRef actorPongRef = actorSystem.actorOf(ActorPong.props(), "actorPong");
+        SpringExtension.getInstance().get(actorSystem).initialize(context);
+        //  ActorRef actorPongRef = actorSystem.actorOf(ActorPong.props(), "actorPong");
+        ActorRef actorPongRef = actorSystem.actorOf(SpringProps.create(actorSystem, ActorPong.class), "actorPong");
 
         actorSystem.getWhenTerminated();
     }
+
 }
